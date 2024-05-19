10eb48a86766b2c5ae2f526de16f3f1f9c2abe88
chi1lyy <julia_ershova2@mail.ru>
2024-05-19 16:10:59 +0300

    Add files via upload

---
+++ actions.js
@@ -0,0 +1,6 @@
+let one = document.querySelector(".one")
+    one.addEventListener('click', function (){
+        document.getElementById("image").style.display = "none";
+        document.getElementById("love").style.display = "block"}
+ )
+ 
