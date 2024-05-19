b2c5652dbdb0c1fd84c76e8571aefded0124d818
chi1lyy <julia_ershova2@mail.ru>
2024-05-19 16:27:09 +0300

    Delete actions.js

--- actions.js
+++
@@ -1,6 +0,0 @@
-let one = document.querySelector(".one")
-    one.addEventListener('click', function (){
-        document.getElementById("image").style.display = "none";
-        document.getElementById("love").style.display = "block"}
- )
- 
